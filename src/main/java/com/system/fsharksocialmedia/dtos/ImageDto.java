5ab65bd49b3a16d336ac6689f8a2558600cde8c6
thuann17 <117891819+thuann17@users.noreply.github.com>
2024-10-29 12:31:17 +0700

    image

---
+++ src/main/java/com/system/fsharksocialmedia/dtos/ImageDto.java
@@ -0,0 +1,18 @@
+package com.system.fsharksocialmedia.dtos;
+
+import lombok.Data;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Data
+public class ImageDto implements Serializable {
+    Integer id;
+    String image;
+    Instant createdate;
+    String avatarrurl;
+    String coverurl;
+    Boolean status;
+    UserDto username;
+}
