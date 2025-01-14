2bdf7fa38bd318eafb027b594ee4542c4fda0011
thuysvy <150228593+thuysvy@users.noreply.github.com>
2025-01-14 17:45:04 +0700

    Update TripRepository.java

--- src/main/java/com/system/fsharksocialmedia/repositories/TripRepository.java
+++ src/main/java/com/system/fsharksocialmedia/repositories/TripRepository.java
@@ -7,10 +7,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface TripRepository extends JpaRepository<Trip, Integer> {
     @Query("SELECT COUNT(p) FROM Trip p WHERE " +
             "(:year IS NULL OR FUNCTION('YEAR', p.createdate) = :year) AND " +
             "(:month IS NULL OR FUNCTION('MONTH', p.createdate) = :month)")
     Integer countTripsByYearAndMonth(@Param("year") Integer year, @Param("month") Integer month);
-
+    @Query("SELECT t , p, ut FROM Trip t " +
+            "JOIN t.usertrips ut " +
+            "JOIN ut.userid u " +
+            "JOIN t.placetrips pt " +
+            "JOIN pt.placeid p " +
+            "WHERE u.username = :username")
+    List<Trip> findTripsByUsernameAndPlaceId(String username);
 }
